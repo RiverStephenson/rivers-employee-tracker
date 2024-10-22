25a6252d33d07b943ae7fdb71aba55571aafc1a9
RiverStephenson <riverstephenson06@outlook.com>
2024-10-22 17:03:45 -0500

    added most of my database

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+\c postgres
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+\c movie_db
+
+CREATE TABLE department(
+id SERIAL PRIMARY KEY,
+dep_name VARCHAR(30)
+);
+
+CREATE TABLE job_role (
+id SERIAL PRIMARY KEY.
+title VARCHAR(40) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INTEGER NOT NULL
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+id SERIAL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+job_role_id INTEGER NOT NULL
+manager_id INTEGER
+);
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO
+    department (dep_name)
+VALUES
+    ('Software'),
+    ('Mechanics'),
+    ('Security'),
+    ('Engineering');
+
+INSERT INTO
+    job_role (title, salary, dep_name)
+VALUES
+    ('Software Engineer Intern', 55000, 1),
+    ('Software Quality Engineer', 120000, 1),
+    ('Full Stack Software Engineer', 127000, 1),
+    ('Devops Engineer', 146000, 1),
+    ('Lead Software Engineer', 157000, 1),
+    ('Software Engineering Manager', 180000, 1),
+    ('Manufacturing Planner', 80000, 2),
+    ('Manufacturing Engineer', 92000, 2),
+    ('Mechanical Engineer', 147000, 2),
+    ('Mechanical Enineering Manager', 172000, 2),
+    ('Security Intern', 41000, 3),
+    ('Systems Security Engineer', 134000),
+    ('CyberSecurity Manager', 156000, 3),
+    ('Senior Analyst', 122000, 4),
+    ('Systems Architect', 254000, 4),
+    ('Lead Systems Engineer', 161000, 4),
+    ('Systems Engineering Manager', 172000, 4),
