bae45e526d7d4c21f004ea5e732ba96e5b056a2f
RiverStephenson <riverstephenson06@outlook.com>
2024-10-22 17:54:08 -0500

    added employees and roles

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS employee_db;
 
 CREATE DATABASE employee_db;
 
-\c movie_db
+\c employee_db
 
 CREATE TABLE department(
 id SERIAL PRIMARY KEY,
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,22 +7,51 @@ VALUES
     ('Engineering');
 
 INSERT INTO
-    job_role (title, salary, dep_name)
+    job_role (title, salary, dep_name, manager)
 VALUES
-    ('Software Engineer Intern', 55000, 1),
-    ('Software Quality Engineer', 120000, 1),
-    ('Full Stack Software Engineer', 127000, 1),
-    ('Devops Engineer', 146000, 1),
-    ('Lead Software Engineer', 157000, 1),
-    ('Software Engineering Manager', 180000, 1),
-    ('Manufacturing Planner', 80000, 2),
-    ('Manufacturing Engineer', 92000, 2),
-    ('Mechanical Engineer', 147000, 2),
-    ('Mechanical Enineering Manager', 172000, 2),
-    ('Security Intern', 41000, 3),
-    ('Systems Security Engineer', 134000),
-    ('CyberSecurity Manager', 156000, 3),
-    ('Senior Analyst', 122000, 4),
-    ('Systems Architect', 254000, 4),
-    ('Lead Systems Engineer', 161000, 4),
-    ('Systems Engineering Manager', 172000, 4),
+    ('Software Engineer Intern', 55000, 1, 'Conrad Williams'),
+    ('Software Quality Engineer', 120000, 1, 'Conrad Williams'),
+    ('Full Stack Software Engineer', 127000, 1, 'Conrad Williams'),
+    ('Devops Engineer', 146000, 1, 'Conrad Williams'),
+    ('Lead Software Engineer', 157000, 1, 'Conrad Williams'),
+    ('Software Engineering Manager', 180000, 1, 'N/A'),
+    ('Manufacturing Planner', 80000, 2, 'Sonny George'),
+    ('Manufacturing Engineer', 92000, 2, 'Sonny George'),
+    ('Mechanical Engineer', 147000, 2, 'Sonny George'),
+    ('Mechanical Enineering Manager', 172000, 2, 'N/A'),
+    ('Security Intern', 41000, 3, 'Marcus Knox'),
+    ('Systems Security Engineer', 134000, 3, 'Marcus Knox'),
+    ('CyberSecurity Manager', 156000, 3, 'Marcus Knox'),
+    ('Senior Analyst', 122000, 4, 'Silas Rios'),
+    ('Systems Architect', 254000, 4, 'Silas Rios'),
+    ('Lead Systems Engineer', 161000, 4, 'Silas Rios'),
+    ('Systems Engineering Manager', 172000, 4, 'N/A'),
+INSERT INTO
+    employees (first_name, last_name, job_role)
+VALUES
+    ('Sharlene', 'Dunn', 2,)
+    ('Norberto', 'Boyle', 3,)
+    ('Marcelino', 'Frost', 1,)
+    ('Fern', 'Costa', 1,)
+    ('Virgil', 'Phillips', 4,)
+    ('Margarete', 'Lester', 4,)
+    ('Stanford', 'Cooley', 5,)
+    ('Conrad', 'Williams', 6,)
+    ('Omer', 'House', 7,)
+    ('Jarret', 'Smith', 8,)
+    ('Darlene', 'Lozano', 7,)
+    ('Alan', 'Wise', 8,)
+    ('Emilee', 'Sheldon', 9,)
+    ('Donn', 'Vasquez', 9,)
+    ('Ivan', 'Henry', 9,)
+    ('Sonny', 'George', 10,)
+    ('William', 'Stafford', 11,)
+    ('Don', 'Juarez', 11,)
+    ('Violet', 'Golden', 12,)
+    ('Marcus', 'Knox', 13,)
+    ('Beth', 'Bradshaw', 14,)
+    ('Melinda', 'Ross', 14,)
+    ('Louis', 'Miranda', 15,)
+    ('Vicky', 'Carter', 15,)
+    ('Gus', 'Humphrey', 16,)
+    ('Silas', 'Rios', 17,)
